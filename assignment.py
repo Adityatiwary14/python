0ea80a0816343b33c67d39c1e7e1b0edbf9114dd
Aditya Tiwari <adityaat1902@gmail.com>
2022-09-17 19:51:47 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+n=int(input("enter your number = "))
+if(n%2==1):
+    print("weird")
+if(n%2==0 and n>=2 and n<=5):
+    print("not weird")
+if(n%2==0 and n>=6 and n<=20):
+    print("weird")
+if(n%2==0 and n>=20):
+    print("not weird")        
+
+
