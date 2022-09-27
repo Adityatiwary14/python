dac577376ef3dde893bb4429bd0bfe2bd709f22d
Aditya Tiwari <adityaat1902@gmail.com>
2022-09-27 19:34:44 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,13 @@
+l=[10,20,30,40,'aditya','tiwari',50,60]
+print(l[0])
+print(l[1])
+print(l[0:6])
+print(l[:4])
+print(l[:])
+print(l[1:])
+print(l[-1])
+print(l[-1:0])
+print(l[:-3])
+print(l[-1:-8])
+print(l[0:-5])
+print(l[8:0])
