f20f4b2f9e04cbc502454d025ea3b46d753a523c
Aditya Tiwari <adityaat1902@gmail.com>
2022-11-20 23:00:15 +0530

    Add files via upload

---
+++ sum_using_fun.py
@@ -0,0 +1,8 @@
+#.sum of two numbers using fun
+
+num1=int(input("enter your first num : "))
+num2=int(input("enter your second num : "))
+def sum():
+    addition=num1 + num2
+    print("summ of numbers = ",addition)
+sum()
